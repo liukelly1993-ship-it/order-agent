7b321dac195bb121576eff13c26fc6fd28f8eb30
万佳恒 <295787521+liukelly1993-ship-it@users.noreply.github.com>
2026-09-13 15:47:41 +0800

    feat(agent): tools-mysql查特色菜

--- agent/langchain-assistant.py
+++ agent/langchain-assistant.py
@@ -7,12 +7,14 @@ from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 from langgraph.checkpoint.memory import InMemorySaver
 
+from agent.tools.tools import search_dishes
+
 ROOT_PATH = Path(__file__).parent.parent
 
 def get_agent():
     checkpointer = InMemorySaver()
-    model = init_chat_model('deepseek-chat')
-    tools = []
+    model = init_chat_model('deepseek-v4-flash')
+    tools = [search_dishes]
     with open(str(ROOT_PATH/'agent/prompt/prompt.txt'),'r',encoding='utf-8') as f:
         system_prompt = f.read()
     agent = create_agent(
@@ -26,7 +28,8 @@ def get_agent():
 if __name__ == '__main__':
     agent = get_agent()
     config = {'configurable':{'thread_id': str(uuid.uuid4())}}
-    input = {'messages':[HumanMessage("你好")]}
-    result = agent.invoke(config, config)
+    input = {'messages':[HumanMessage("你好,你们餐厅有什么特色菜?")]}
+    result = agent.invoke(input, config)
+    print(result)
     print(result['messages'][-1].content)
 
---
+++ agent/tools/tools.py
@@ -0,0 +1,44 @@
+import os
+
+import main
+import pymysql
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from pymysql.cursors import DictCursor
+
+
+load_dotenv()
+
+
+@tool
+def search_dishes():
+    '''搜索查询餐厅的菜品,返回餐厅菜品数据'''
+    # 连接到数据库
+    with pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_DATABASE"),
+        port=int(os.getenv("DB_PORT")),
+        charset=os.getenv("DB_CHARSET"),
+    ) as conn:
+        # 创建一个新游标来执行查询
+        with conn.cursor(DictCursor) as cursor:
+            cursor.execute('''
+                select 
+                    dish_name as 菜品名称, 
+                    price as 价格, 
+                    description as 描述, 
+                    category as 菜品类别, 
+                    spice_level as 麻辣程度, 
+                    flavor as 口味, 
+                    main_ingredients as 主料, 
+                    cooking_method as 烹饪方法, 
+                    is_vegetarian as 是否素食, 
+                    allergens as 过敏源
+                    from menu_items
+                where 
+                    is_featured = 1
+            ''')
+            special_dishes = cursor.fetchall()
+    return special_dishes
